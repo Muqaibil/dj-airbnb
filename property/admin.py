97389b2ef8c431e928d13e34e2132e1c63142ee9
Muqaibil <mam_770@yahoo.com>
2020-11-04 15:56:38 +0300

    Create the property models, and STATIC FILES and MEDIA setttings, and models registeration to Admin Page

--- property/admin.py
+++ property/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from . import models
+
+
+class RoomImageTabular(admin.TabularInline):
+    #This is Tabular Admin class is to include a room images model in a tabular view
+    #and then merge into the Room admin page
+    model = models.RoomImage
+
+class RoomAdmin(admin.ModelAdmin): 
+    #This customized admin is created to merge more than a model registeration into one page. 
+    #for example, room and roomImages Here
+
+    list_display = ['name','location','price']
+    inlines = [RoomImageTabular,] 
+    
+    #Tabular image container is included in the RoomAdmin page as this class is register and called
+    #within the registeration method of model Room.
+
+
+
+admin.site.register(models.Room, RoomAdmin) #you can add a class "RoomAdmin fro example" to change the model aspects like display fields etc.
+admin.site.register(models.Category)
+admin.site.register(models.RoomReview)
+admin.site.register(models.RoomBook)
+admin.site.register(models.RoomImage)
--- property/models.py
+++ property/models.py
@@ -8,19 +8,37 @@ class Room(models.Model):
     description = models.TextField(max_length=10000)
     location = models.CharField(max_length=100)
     image = models.ImageField(upload_to='property/')
+    category = models.ForeignKey('Category', related_name='room_category', on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.name
 
 class RoomImage(models.Model):
     room = models.ForeignKey(Room, related_name='room_image', on_delete=models.CASCADE)
     image = models.ImageField(upload_to='room_image/')
 
+    def __str__(self):
+        return str(self.room)
+
+
 class Category(models.Model):
     name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
 
 class RoomReview(models.Model):
     room = models.ForeignKey(Room, related_name='room_review', on_delete=models.CASCADE)
     rate = models.IntegerField(default=0)
     feedback = models.TextField(max_length=300, blank=True, null=True)
+
+    def __str__(self):
+        return str(self.room)
+
 
 class RoomBook(models.Model):
     room = models.ForeignKey(Room, related_name='room_book', on_delete=models.CASCADE)
@@ -30,3 +48,6 @@ class RoomBook(models.Model):
     to_date = models.DateField(default=timezone.now)
     guest = models.IntegerField(default=1)
     childern = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
